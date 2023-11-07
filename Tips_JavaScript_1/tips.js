532798fb6632f85419b52a4dbb57c615ff6386e6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-11-07 20:00:30 +0600

    added to swaping  in value

---
+++ Tips_JavaScript_1/tips.js
@@ -0,0 +1,15 @@
+//  swaping js value
+let a = 1;
+let b = 2;
+
+// console.log(a, b);
+
+let tap = a;
+a = b;
+b = tap;
+
+// console.log(a, b);
+
+let array = [1, 2, 3, 4, 5];
+array[0] = 5;
+console.log(array);
